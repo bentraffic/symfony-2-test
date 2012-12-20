f1bee8b073a40d444bc391385e3b90495fe2a71a
Ben Eppel <ben@trafficdigital.com>
2012-12-20 13:26:13 +0000

    added comments to bash

--- conf/web-boot.sh
+++ conf/web-boot.sh
@@ -1,12 +1,12 @@
 sed -i 's/Listen 80/Listen '$PORT'/' /app/apache/conf/httpd.conf
-sed -i 's/^DocumentRoot/# DocumentRoot/' /app/apache/conf/httpd.conf
-sed -i 's/^ServerLimit 1/ServerLimit 8/' /app/apache/conf/httpd.conf
-sed -i 's/^MaxClients 1/MaxClients 8/' /app/apache/conf/httpd.conf
 
+#copy over any environment variables that are set by heroku
 for var in `env|cut -f1 -d=`; do
   echo "PassEnv $var" >> /app/apache/conf/httpd.conf;
 done
 echo "Include /app/www/conf/httpd/*.conf" >> /app/apache/conf/httpd.conf
+
+#setup error log files and access log files
 touch /app/apache/logs/error_log
 touch /app/apache/logs/access_log
 tail -F /app/apache/logs/error_log &
@@ -15,5 +15,4 @@ export LD_LIBRARY_PATH=/app/php/ext
 export PHP_INI_SCAN_DIR=/app/www
 echo "Launching apache"
 exec /app/apache/bin/httpd -DNO_DETACH
-exec /app/bin/php www/app/console assets:install /app/www/web/
 exec chmod -R 775 /app/www/web
